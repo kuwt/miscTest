7afab2cee00188538bb537ab46c7f51ae6b67787
kuwingto <kuwingto@gmail.com>
2018-08-23 00:16:03 +0800

    add details for RuleOfFiveTest.cpp

--- misc/RuleOfFiveTest/RuleOfFiveTest.cpp
+++ misc/RuleOfFiveTest/RuleOfFiveTest.cpp
@@ -1,8 +1,13 @@
-// RuleOfFiveTest.cpp : Defines the entry point for the console application.
-//
+/**
+RuleOfFiveTest.cpp
+Purpose: Illustrate the Rule of Five for class design
+and how the calling pattern will invoke each of the rule of five methods
 
+@author Ku Wing To
+@version 1.0 23/08/2018
+*/
 #include "stdafx.h"
-
+#include <iostream>
 
 struct resource
 {
@@ -14,25 +19,32 @@ class foo
 public:
 	// constructor
 	foo()
-		: p{ new resource{} }
-	{ }
+	{
+		std::cout << "constructor" << "\n";
+		p = new resource();
+	}
 
 	// copy constructor
 	foo(const foo& other)
-		: p{ new resource{ *(other.p) } }
-	{ }
+	{ 
+		std::cout << "copy constructor" << "\n";
+		p = new resource(*(other.p));
+	}
 
 	// move constructor
 	foo(foo&& other)
-		: p{ other.p }
 	{
+		std::cout << "move constructor" << "\n";
+		p = other.p;
 		other.p = nullptr;
 	}
 
 	// assignment operator
 	foo& operator=(const foo& other)
 	{
-		if (&other != this) {
+		std::cout << "assignment operator" << "\n";
+		if (&other != this)
+		{
 			delete p;
 			p = nullptr;
 			p = new resource{ *(other.p) };
@@ -43,15 +55,19 @@ public:
 	// move assignment operator
 	foo& operator=(foo&& other)
 	{
-		if (&other != this) {
+		std::cout << "move assignment operator" << "\n";
+		if (&other != this) 
+		{
 			delete p;
 			p = other.p;
 			other.p = nullptr;
 		}
 		return *this;
 	}
+	// destructor
 	~foo()
 	{
+		std::cout << "destructor" << "\n";
 		delete p;
 	}
 private:
@@ -59,11 +75,21 @@ private:
 };
 
 
-
-
 int main()
 {
+	foo st1;   
+	foo st2(st1);   // copy constructor
+	foo st3 = st1;   // copy constructor
 
+	foo st4;
+	st4 = foo();    // move assignment operator
+
+	foo st5;
+	foo st6(std::move(st5));  // move constructor
+
+	foo st7;
+	foo st8;
+	st8 = st7;  // assignment operator
     return 0;
 }
 
